f3b6bfddd47f8cd0ddf9ea0de9b704022fbc5479
Diego De Sogos <diegodesogos@gmail.com>
2025-09-22 14:56:02 -0300

    added api/index.js as entry point for Vercel cloud

---
+++ api/index.js
@@ -0,0 +1,6 @@
+// This file serves as the entry point for Vercel Functions
+// It imports your TypeScript Express app and exports it for Vercel
+
+import app from '../dist/server/index.js';
+
+export default app;
